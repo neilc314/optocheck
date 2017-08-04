0a458f02f05a7e38c2dce83c073bb3076872591d
neilc314 <neil.chowdhury@outlook.com>
2017-08-04 14:05:12 -0700

    Added red and green sailboats

--- src/pages/redgreen/redgreen.ts
+++ src/pages/redgreen/redgreen.ts
@@ -32,7 +32,51 @@ export class RedgreenPage {
 
     this.renderer.setElementAttribute(this.canvasElement, 'width', this.platform.width() + "");
     this.renderer.setElementAttribute(this.canvasElement, 'height', this.platform.height() + "");
-
+    
+    let ctx = this.canvasElement.getContext('2d');
+    this.drawSailboat(ctx, 'red', 0, 0);
+    this.drawSailboat(ctx, 'green', 50, 0);
   }
+
+  drawSailboat(context, color, x, y) {
+    if (color == 'red') {
+      context.fillStyle = "rgb(255, 0, 0)";
+      context.strokeStyle = "rgb(255, 0, 0)";
+    } else {
+      context.fillStyle = "rgb(0, 255, 0)";
+      context.strokeStyle = "rgb(0, 255, 0)";
+    }
+		context.lineWidth = 2;
+		context.beginPath();
+		context.moveTo(25+x, 160+y);
+		context.lineTo(70+x, 180+y);
+		context.lineTo(150+x, 180+y);
+		context.lineTo(175+x, 160+y);
+		context.closePath();
+		context.stroke();
+		context.fill();
+		
+		// pole
+		context.beginPath();
+		context.lineWidth = 5;
+		context.moveTo(100+x,160+y);
+		context.lineTo(100+x,50+y);
+		context.stroke();
+		
+		// left sail
+		context.beginPath();
+		context.moveTo(96+x,50+y);
+		context.lineTo(50+x,155+y);
+		context.lineTo(90+x,140+y);
+		context.fill();
+		
+		// right sail
+		context.beginPath();
+		context.moveTo(104+x,50+y);
+		context.lineTo(160+x,150+y);
+		context.lineTo(110+x,150+y);
+		context.fill();
+}
+
 
 }
