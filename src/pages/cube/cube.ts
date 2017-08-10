8f7db456bea295a58bf6b5b74d692b7c462b1736
neilc314 <neil.chowdhury@outlook.com>
2017-08-10 16:26:28 -0700

    Added basic drawing functions

--- src/pages/cube/cube.ts
+++ src/pages/cube/cube.ts
@@ -16,6 +16,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class CubePage {
   @ViewChild('glCanvas') canvas: any;
   canvasElement: any;
+  ctx: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public renderer: Renderer, public platform: Platform) {
   }
@@ -30,6 +31,28 @@ export class CubePage {
 
     this.renderer.setElementAttribute(this.canvasElement, 'width', this.platform.width() + "");
     this.renderer.setElementAttribute(this.canvasElement, 'height', this.platform.height() + "");
+    this.ctx = this.canvasElement.getContext('2d');
+    this.draw();
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.platform.width(), this.platform.height());
+    this.drawCenterLine();
+    this.drawBall(100, 100);
+  }
+
+  drawCenterLine() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(Math.floor(this.platform.width() / 2), 0);
+    this.ctx.lineTo(Math.floor(this.platform.width() / 2), Math.floor(this.platform.height()));
+    this.ctx.stroke();
+  }
+
+  drawBall(x: number, y: number) {
+    this.ctx.beginPath();
+    this.ctx.arc(x, y, 25, 0, 2 * Math.PI, false);
+    this.ctx.fillStyle = 'lightblue';
+    this.ctx.fill();
   }
 
 
